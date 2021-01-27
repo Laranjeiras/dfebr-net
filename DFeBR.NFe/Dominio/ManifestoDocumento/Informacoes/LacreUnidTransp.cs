52eabc3d0cee74a47c16d5cbe1178ed51780839c
Alexandre Laranjeiras <laranja22@hotmail.com>
2021-01-27 02:20:14 -0300

    Informações de desenvolvimento.

--- DFeBR.NFe/Dominio/ManifestoDocumento/Informacoes/LacreUnidTransp.cs
+++ DFeBR.NFe/Dominio/ManifestoDocumento/Informacoes/LacreUnidTransp.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿// =============================================================
+// DFeBR - Documentos Fiscais Eletrônicos em .Net
+// Projeto: DFeBR.NFe
+// Autor: 
+// Alexandre Laranjeiras (laranja22@hotmail.com)
+// Data Criação: 27/10/2020
+// Todos os direitos reservados
+// =============================================================
+
 using System.Xml.Serialization;
 
 namespace DFeBR.EmissorNFe.Dominio.ManifestoDocumento.Informacoes
--- DFeBR.NFe/Dominio/ManifestoDocumento/Informacoes/infCIOT.cs
+++ DFeBR.NFe/Dominio/ManifestoDocumento/Informacoes/infCIOT.cs
@@ -1,4 +1,13 @@
-﻿namespace DFeBR.EmissorNFe.Dominio.ManifestoDocumento.Informacoes
+﻿// =============================================================
+// DFeBR - Documentos Fiscais Eletrônicos em .Net
+// Projeto: DFeBR.NFe
+// Autor: 
+// Alexandre Laranjeiras (laranja22@hotmail.com)
+// Data Criação: 27/10/2020
+// Todos os direitos reservados
+// =============================================================
+
+namespace DFeBR.EmissorNFe.Dominio.ManifestoDocumento.Informacoes
 {
     public class InfCIOT
     {
